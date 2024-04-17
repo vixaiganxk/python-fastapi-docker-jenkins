370439573afe393f83a8a8b5a74a5497775a7017
vixaiganxk <vijayganeshkaruppasamy@gmail.com>
2024-04-17 18:46:29 +0530

    FastAPI app commit

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    name = "Vijay"
+    return {"message": f"Hello {name}"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
-    return {"item_id": item_id}
+    return {"item_id": int(item_id)}
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, port=8000, host="0.0.0.0")
